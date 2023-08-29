d7b455401d0a99a8ee99c974a8bbb69258531a29
Zachary Turner <zakkturner@yahoo.com>
2023-08-28 22:00:42 -0400

    finished vue form rough draft

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -29,7 +29,7 @@ class ProjectController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Projects/Create');
     }
 
     /**
---
+++ resources/js/Pages/Projects/Create.vue
@@ -0,0 +1,101 @@
+<template>
+    <authenticated-layout>
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="bg-white p-6">
+                        <h1>Create New Post</h1>
+                        <form @submit.prevent="submit">
+                            <div class="flex flex-col mb-4">
+                                <label for="name">Name</label>
+                                <input
+                                    type="text"
+                                    name="name"
+                                    id=""
+                                    v-model="name"
+                                />
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <label for="description">Description</label>
+                                <textarea
+                                    type="text"
+                                    name="description"
+                                    id=""
+                                    v-model="description"
+                                ></textarea>
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <label for="short-description"
+                                    >Short Description</label
+                                >
+                                <textarea
+                                    type="text"
+                                    name="short-description"
+                                    id=""
+                                    v-model="short_description"
+                                ></textarea>
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <label for="img_src">Image</label>
+                                <input
+                                    type="file"
+                                    name="img_src"
+                                    id=""
+                                    @input="
+                                        formData.img_src =
+                                            $event.target.files[0]
+                                    "
+                                />
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <label for="categories">Category</label>
+                                <select name="categories" v-model="categories">
+                                    <option
+                                        value="none"
+                                        selected
+                                        disabled
+                                        hidden
+                                    >
+                                        Select a Category
+                                    </option>
+                                    <option value="frontend">Frontend</option>
+                                    <option value="ecommerce">Ecommerce</option>
+                                    <option value="php">PHP</option>
+                                </select>
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <label for="site">Project Url</label>
+                                <input
+                                    type="text"
+                                    name="site"
+                                    id=""
+                                    v-model="site"
+                                />
+                            </div>
+                            <div class="flex flex-col mb-4">
+                                <input type="submit" value="Submit" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </authenticated-layout>
+</template>
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import { useForm } from "@inertiajs/vue3";
+
+const formData = useForm({
+    name: "",
+    description: "",
+    short_description: "",
+    img_src: null,
+    categories: "",
+    site: "",
+});
+
+function submit() {
+    formData.post(route("projects.store"));
+}
+</script>
