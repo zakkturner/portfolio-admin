5858c0a3674a81b78162b2c14952e98597b96992
zakkturner <zakkturner@yahoo.com>
2024-11-16 12:19:36 -0500

    delete modal working. now testing for delete functionalityu

--- resources/js/Pages/Submissions/Index.vue
+++ resources/js/Pages/Submissions/Index.vue
@@ -6,9 +6,17 @@ import ProjectCard from "@/Components/projects/ProjectCard.vue";
 import DangerButton from "@/Components/DangerButton.vue";
 import SecondaryButton from "@/Components/SecondaryButton.vue";
 import NavLink from "@/Components/NavLink.vue";
+import Modal from "@/Components/Modal.vue";
+import {ref} from "vue";
 defineProps({
   submissions: Object
-})
+});
+
+const modalOpen = ref(false)
+function handleDelete(){
+  modalOpen.value = !modalOpen.value;
+}
+
 </script>
 
 <template>
@@ -42,12 +50,27 @@ defineProps({
             </span>
             <span class="flex  items-center">
               <a :href="`mailto:${submission.email}`" class="mr-4 rounded bg-white px-4 py-1">Reply</a>
-              <danger-button>Delete</danger-button>
+              <danger-button @click="modalOpen = true">Delete</danger-button>
             </span>
           </li>
         </ul>
     </div>
+   <Teleport to="body">
+      <modal class="flex items-center" :show="modalOpen">
+        <div class="flex flex-col items-center w-full py-4">
+          <div class="">
+            <h2>Are You Sure?</h2>
+          </div>
+          <div class="">
+            <danger-button class="mr-4">Confirm</danger-button>
+            <button class="rounded-full" @click="modalOpen = false">
+              Cancel
+            </button>
+          </div>
 
+        </div>
+      </modal>
+   </Teleport>
   </AuthenticatedLayout>
 </template>
 
--- tests/Feature/Controllers/FormSubmissionTest.php
+++ tests/Feature/Controllers/FormSubmissionTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Controllers\FormSubmission;
 
+use App\Models\FormSubmission;
 use Inertia\Testing\AssertableInertia;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,7 +11,8 @@ use Tests\TestCase;
 class FormSubmissionTest extends TestCase
 {
 
-
+    use RefreshDatabase;
+    protected $seed = true;
     public function test_form_submissions_return_a_200_response()
     {
         $user = User::factory()->create();
@@ -28,12 +30,21 @@ class FormSubmissionTest extends TestCase
         $user = User::factory()->create();
         $this->actingAs($user);
         $response = $this->get(route('submissions.index'));
-
+        $submissions = FormSubmission::factory(10)->create();
         $response->assertInertia(fn (AssertableInertia $inertia) =>
         $inertia->component('Submissions/Index')
             ->has('submissions')
+            ->count('submissions',10)
         );
 
+
+
+    }
+    public function test_form_submissions_can_be_deleted(){
+        $user = User::factory()->create();
+        $this->actingAs($user);
+        $submissions = FormSubmission::factory(10)->create();
+        $response = $this->delete(route('submissions.destroy', ['id' => 1]))->assertOk()->count($submissions, 9);
 
     }
 }
